97c251c394ef6540e2c7906bca8508a3f9d5eaac
PavelPoroskov <pavl1853@gmail.com>
2019-08-01 17:50:40 +0300

    Updates

---
+++ precache-manifest.2d4fbafe887380f684108bb9eeef7c98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f5ada81acfb40f8bcdbca87f15cb9b",
+    "url": "/test-task-pw-transfer/index.html"
+  },
+  {
+    "revision": "511dcac37a3282a69745",
+    "url": "/test-task-pw-transfer/static/css/main.2e6f2860.chunk.css"
+  },
+  {
+    "revision": "9d8b35e720b8cbc6f167",
+    "url": "/test-task-pw-transfer/static/js/2.6218e9e8.chunk.js"
+  },
+  {
+    "revision": "511dcac37a3282a69745",
+    "url": "/test-task-pw-transfer/static/js/main.713814da.chunk.js"
+  },
+  {
+    "revision": "137ac8ad29cc646ae17f",
+    "url": "/test-task-pw-transfer/static/js/runtime~main.1d6d7d1d.js"
+  }
+]);
--- src/config.ts
+++
@@ -1,1 +0,0 @@
-export const BASE_URL = 'http://193.124.114.46:3001';
